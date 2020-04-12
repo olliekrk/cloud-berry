75d10bc4fd9ac9b57c40b2bbfa6cb24908c1efe0
olliekrk <krolik.olgierd@gmail.com>
2020-04-12 22:22:36 +0200

    Fix trusted packages in kafka streams config

--- src/main/java/com/cloudberry/cloudberry/config/kafka/KafkaStreamsConfig.java
+++ src/main/java/com/cloudberry/cloudberry/config/kafka/KafkaStreamsConfig.java
@@ -1,23 +1,41 @@
 package com.cloudberry.cloudberry.config.kafka;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.kafka.support.serializer.JsonSerde;
+
+import java.util.HashMap;
 
 @Configuration
 @EnableKafkaStreams
 public class KafkaStreamsConfig {
-//
-//    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-//    public KafkaStreamsConfiguration kStreamsConfigs() {
-//        Map<String, Object> configs = new HashMap<>();
-//        return new KafkaStreamsConfiguration(configs);
-//    }
 
-//    @Bean
-//    public KStream<?, ?> someStream(StreamsBuilder kStreamBuilder) {
-//        KStream<?, ?> stream = kStreamBuilder.stream(KafkaTopics.PROBLEM_DEFINITION_TOPIC);
-//        return stream;
-//    }
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration kStreamsConfigs(KafkaProperties kafkaProperties,
+                                                     @Value("${spring.application.name}") String applicationName) {
+        var properties = new HashMap<String, Object>(kafkaProperties.getProperties());
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationName);
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class);
+        return new KafkaStreamsConfiguration(properties);
+    }
+
+    @Bean
+    public KStream<String, Object> kStream(StreamsBuilder kStreamBuilder) {
+        KStream<String, Object> stream = kStreamBuilder.stream(KafkaTopics.Logs.WORKPLACE_TOPIC);
+        // how to use: provide processors implementation
+        stream.foreach((_key, value) -> System.out.println("KStream with WorkplaceEvent: " + value));
+        return stream;
+    }
 
 }
