30532ad4db969b15de739e97d8ee061ad40cff66
olliekrk <krolik.olgierd@gmail.com>
2020-08-16 15:20:53 +0200

    python-api-refactor: plot series color based on series_name hash

--- cloudberry-py/cloudberry/api/plots.py
+++ cloudberry-py/cloudberry/api/plots.py
@@ -18,10 +18,14 @@ class DataSeriesPlots:
              y_field: str,
              figsize=DEFAULT_PLOT_SIZE,
              kind=DEFAULT_PLOT_KIND,
-             color=DEFAULT_PLOT_COLOR,
              title=None) -> plt.Axes:
         df = self.series.as_data_frame
-        axes = df.plot(x=x_field, y=y_field, color=color, title=title, figsize=figsize, kind=kind)
+        axes = df.plot(x=x_field,
+                       y=y_field,
+                       color=DataSeriesPlots._get_series_color(self.series),
+                       title=title,
+                       figsize=figsize,
+                       kind=kind)
         plt.show()
         return axes
 
@@ -32,13 +36,28 @@ class DataSeriesPlots:
                 y_field: str,
                 figsize=DEFAULT_PLOT_SIZE,
                 kind=DEFAULT_PLOT_KIND,
-                color=DEFAULT_PLOT_COLOR,
                 title=None) -> plt.Axes:
         figure, axes = plt.subplots(figsize=figsize)
         if title:
             axes.set_title(title)
         for ds in series:
             df = ds.as_data_frame
-            axes.plot(df[x_field], df[y_field], label=ds.series_name)
+            axes.plot(df[x_field],
+                      df[y_field],
+                      label=ds.series_name,
+                      color=DataSeriesPlots._get_series_color(ds))
         axes.legend()
         return axes
+
+    @staticmethod
+    def _get_series_color(series: DataSeries):
+        max_hex = 256
+
+        def rgb_scaled(v):
+            return float(v % max_hex) / (max_hex - 1)
+
+        series_name_hash: int = hash(series.series_name) % max_hex ** 3
+        red = rgb_scaled(series_name_hash)
+        green = rgb_scaled(series_name_hash / max_hex)
+        blue = rgb_scaled(series_name_hash / max_hex ** 2)
+        return red, green, blue
