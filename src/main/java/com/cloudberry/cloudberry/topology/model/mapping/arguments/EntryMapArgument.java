9e56548ce8d224dec40ee973a0c02ca77a436abd
mimagiera <michalmagierra@gmail.com>
2020-11-06 18:29:29 +0100

    #71 fix rest for MapNode

--- src/main/java/com/cloudberry/cloudberry/topology/model/mapping/arguments/EntryMapArgument.java
+++ src/main/java/com/cloudberry/cloudberry/topology/model/mapping/arguments/EntryMapArgument.java
@@ -1,11 +1,16 @@
 package com.cloudberry.cloudberry.topology.model.mapping.arguments;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class EntryMapArgument implements MappingArgument<EntryMapRecord> {
 
     private final EntryMapRecord value;
+
+    public EntryMapArgument(@JsonProperty("value") EntryMapRecord value) {
+        this.value = value;
+    }
 
     @Override
     public EntryMapRecord getArgument() {
--- src/main/java/com/cloudberry/cloudberry/topology/model/mapping/arguments/MappingArgument.java
+++ src/main/java/com/cloudberry/cloudberry/topology/model/mapping/arguments/MappingArgument.java
@@ -1,5 +1,16 @@
 package com.cloudberry.cloudberry.topology.model.mapping.arguments;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = DoubleArgument.class),
+        @JsonSubTypes.Type(value = EntryMapArgument.class)
+})
 public interface MappingArgument<T> {
+    @JsonIgnore
     T getArgument();
 }
