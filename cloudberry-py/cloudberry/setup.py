dd990124d6231e83de4d460c19261ea9980334b0
olliekrk <krolik.olgierd@gmail.com>
2020-11-29 21:29:53 +0100

    csv-scenario: remove dash from lib name

--- cloudberry-py/cloudberry/setup.py
+++ cloudberry-py/cloudberry/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cloudberry-py",
+    name="cloudberrypy",
     version="0.0.1",
     author="Olgierd Królik & Michał Magiera",
     author_email="dbiszkopter@gmail.com",
